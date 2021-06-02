2720e6a612b4f51cc3bcb02a7155f51e6a702a2b
cebarobot <ceba_robot@outlook.com>
2021-06-02 19:12:19 +0800

    feat: finish order & ticket model insert method

--- web/app/models/Order.php
+++ web/app/models/Order.php
@@ -1,13 +1,46 @@
 <?php
 namespace app\models;
 
+use foundation\Database;
+use app\models\Auth;
+
 class Order extends \foundation\BaseModel {
     public $orderID;
-    public $orderDate;
+    public $orderTime;
     public $status;
     public $ticketList;
 
     public function query() {
         
+    }
+
+    public function insert() {
+        $userName = Auth::user()->userName;
+        $ticketID1 = isset($this->ticketList[0])? $this->ticketList[0]->ticketID : 'null';
+        $ticketID2 = isset($this->ticketList[1])? $this->ticketList[1]->ticketID : 'null';
+        $sql = <<<SQLEOF
+insert into OrderInfo (
+    ticketID1,
+    ticketID2,
+    orderTime,
+    orderStatus,
+    userName
+) values (
+    {$ticketID1},
+    {$ticketID2},
+    now(),
+    'reserved',
+    '{$userName}'
+) returning id, orderTime;
+SQLEOF;
+        $res = Database::query($sql);
+        $resRow = Database::fetchRow($res);
+        $this->orderID = intval($resRow[0]);
+        $this->orderTime = $resRow[1];
+
+        var_dump($this);
+    }
+    public function cancel() {
+
     }
 }
--- web/app/models/Ticket.php
+++ web/app/models/Ticket.php
@@ -1,7 +1,12 @@
 <?php
 namespace app\models;
 
+use foundation\Database;
+use app\models\Auth;
+
+
 class Ticket extends \foundation\BaseModel {
+    public $ticketID;
     public $trainNum;
     public $date;
     public $depSta;
@@ -12,4 +17,36 @@ class Ticket extends \foundation\BaseModel {
     public $seatType;
     public $price;
     public $status;
+
+    public function query() {
+
+    }
+
+    public function insert() {
+        $trainNum = $this->trainNum;
+        $trainDate = $this->date;
+        $depSta = $this->depSta;
+        $arrSta = $this->arrSta;
+        $seatType = $this->seatType;
+        $price = $this->price;
+        $sql = <<<SQLEOF
+insert into TicketInfo (
+    trainNum,
+    trainDate,
+    seatType,
+    departureNum,
+    arrivalNum,
+    price
+) values (
+    '{$trainNum}',
+    '{$trainDate}',
+    '{$seatType}',
+    (select t_stopnum from train where (t_number = '{$trainNum}' and t_station = '{$depSta}')),
+    (select t_stopnum from train where (t_number = '{$trainNum}' and t_station = '{$arrSta}')),
+    '{$price}'
+) returning id;
+SQLEOF;
+        $res = Database::query($sql);
+        $this->ticketID = intval(Database::fetchRow($res)[0]);
+    }
 }
